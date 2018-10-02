b8601423f03c484dc0ab2635689a6acf07a79e2e
karol <ka.lipiec@gmail.com>
2018-10-02 22:01:13 +0200

    test cases for pending events

--- src/test/groovy/io/pillopl/eventsourcing/persistence/CreditCardRepositoryTest.groovy
+++ src/test/groovy/io/pillopl/eventsourcing/persistence/CreditCardRepositoryTest.groovy
@@ -24,6 +24,44 @@ class CreditCardRepositoryTest extends Specification {
             CreditCard loaded = creditCardRepository.load(uuid)
         then:
             loaded.availableLimit() == 90
-            loaded.getPendingEvents().size() == 0
+    }
+
+    def 'should not have pending events after load'() {
+        given:
+            UUID uuid = UUID.randomUUID()
+        when:
+            CreditCard loaded = creditCardRepository.load(uuid)
+        then:
+            loaded.pendingEvents.size() == 0
+    }
+
+    def 'should not have pending events after save'() {
+        given:
+            UUID uuid = UUID.randomUUID()
+        and:
+            CreditCard card = new CreditCard(uuid)
+        and:
+            card.assignLimit(100)
+        when:
+            creditCardRepository.save(card)
+        then:
+            card.pendingEvents.size() == 0
+    }
+
+    def 'should not have pending events after save and load'() {
+        given:
+            UUID uuid = UUID.randomUUID()
+        and:
+            CreditCard card = new CreditCard(uuid)
+        and:
+            card.assignLimit(100)
+        when:
+            creditCardRepository.save(card)
+        and:
+            CreditCard loaded = creditCardRepository.load(uuid)
+        then:
+            loaded.pendingEvents.size() == 0
+        and:
+            card.pendingEvents.size() == 0
     }
 }
